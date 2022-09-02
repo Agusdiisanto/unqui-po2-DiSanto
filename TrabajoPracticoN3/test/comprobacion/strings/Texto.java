c23496447a642a67e225e292af3ffc1d73fcdc87
agusdiisanto <agusdisanto99@gmail.com>
2022-09-02 20:23:38 -0300

    TP3

--- TrabajoPracticoN3/test/comprobacion/strings/Texto.java
+++ TrabajoPracticoN3/test/comprobacion/strings/Texto.java
@@ -1,4 +1,4 @@
-package comprobacionStrings;
+package comprobacion.strings;
 
 public class Texto {
 	
--- TrabajoPracticoN3/test/ejercicio/numeros/CounterTestCase.java
+++ TrabajoPracticoN3/test/ejercicio/numeros/CounterTestCase.java
@@ -1,4 +1,4 @@
-package ejercicioNumeros;
+package ejercicio.numeros;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
