3a9824079872c51d11839cfb9378a859b64680dd
Agusdiisanto <agusdisanto99@gmail.com>
2022-09-07 21:13:28 -0300

    TP4: v1.0.0

---
+++ TrabajoPracticoN4/src/supermercado/Supermercado.java
@@ -0,0 +1,42 @@
+package supermercado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Supermercado {
+	
+	String nombre;
+	String direccion;
+	private List<Producto> productos = new ArrayList<Producto>();
+	
+
+	public Supermercado(String nombreDeSupermercado, String direccionDeSupermercado) {
+		this.nombre = nombreDeSupermercado;
+		this.direccion = direccionDeSupermercado;
+	}
+
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+	
+	public void agregarProducto(Producto productoAgregar) {
+		productos.add(productoAgregar);
+	}
+	
+	public int getCantidadDeProductos() {
+		return productos.size();
+	}
+	
+	
+	public double getPrecioTotal() {
+		double precioTotal = 0;
+		
+		for (Producto producto : productos) {
+			precioTotal = precioTotal + producto.getPrecio();
+		}
+		return precioTotal;
+		
+	}
+	
+}
