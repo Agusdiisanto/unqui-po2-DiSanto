b4087d2b844cea5c62b60a80a0ff8cf1f65e4ffb
Agusdiisanto <agusdisanto99@gmail.com>
2022-10-04 20:05:40 -0300

    TP-Doubles

--- TP-Doubles/src/poker/src/PokerStatus.java
+++ TP-Doubles/src/poker/src/PokerStatus.java
@@ -6,60 +6,54 @@ import java.util.stream.Collectors;
 
 public class PokerStatus {
 	
-	private String carta1;
-	private String carta2;
-	private String carta3;
-	private String carta4;
-	private String carta5;
-	
-	public PokerStatus(String carta1, String carta2, String carta3, String carta4, String carta5) {
-		this.carta1 = carta1;
-		this.carta2 = carta2;
-		this.carta3 = carta3;
-		this.carta4 = carta4;
-		this.carta5 = carta5;
-	}
-
-	public String getCarta1() {
-		return carta1;
-	}
-	
-	public String getCarta2() {
-		return carta2;
-	}
-
-	public String getCarta3() {
-		return carta3;
-	} 
-
-	public String getCarta4() {
-		return carta4;
-	}
-
-	public String getCarta5() {
-		return carta5;
-	}
-	
 	// PARA QUE SEA POKER NECESITO 4 DEL MISMO NUMERO O DEL MISMO PALO || SOLO POR EL MOMENTO DE 
 	public boolean verificar(String carta1, String carta2, String carta3, String carta4, String carta5) {
-		
+	
+		return (this.tieneMismoNumero(carta1, carta2, carta3, carta4, carta5) || this.tieneMismoPalo(carta1, carta2, carta3, carta4, carta5));
+	}
+	
+	public boolean tieneMismoNumero(String carta1, String carta2, String carta3, String carta4, String carta5) {
+
 		List<String> listaDeCartas =  Arrays.asList(carta1,carta2,carta3,carta4,carta5);
 		List<String> cartak =  listaDeCartas.stream().filter(c -> this.esElMismoNumero(carta1, c)).collect(Collectors.toList());
 		List<String> cartax =  listaDeCartas.stream().filter(c -> this.esElMismoNumero(carta2, c)).collect(Collectors.toList());
-		List<String> cartay =  listaDeCartas.stream().filter(c -> this.esElMismoNumero(carta3, c)).collect(Collectors.toList());
-		
-		return (cartak.size() >= 4) || (cartax.size() >= 4) || (cartay.size() >= 4);
-	}
-	
 
-	public boolean esElMismoNumero(String carta1, String carta2) {
-		Character caracteresCarta1 = carta1.charAt(0);
-		Character caracteresCarta2 = carta1.charAt(0);
-		return (caracteresCarta1 == caracteresCarta2) && (carta1.length() == carta2.length()); 		
+		return (cartak.size() == 4 || cartax.size() == 4);
 	}
 	
+	public boolean tieneMismoPalo(String carta1, String carta2, String carta3, String carta4, String carta5) {
+		
+		List<String> listaDeCartas =  Arrays.asList(carta1,carta2,carta3,carta4,carta5);
+		List<String> cartak =  listaDeCartas.stream().filter(c -> this.esElMismoPalo(carta1, c)).collect(Collectors.toList());
+		List<String> cartax =  listaDeCartas.stream().filter(c -> this.esElMismoPalo(carta2, c)).collect(Collectors.toList());
 	
+		return (cartak.size() >= 4 || cartax.size() >= 4);
+	}
+
+	public boolean esElMismoPalo(String carta1, String carta2) {
+		return (this.getPaloDeCarta(carta2) == this.getPaloDeCarta(carta1)); 		
+	}
 	
+	public boolean esElMismoNumero(String carta1, String carta2) {
+		return (this.getValorDeCarta(carta2) == this.getValorDeCarta(carta1)); 		
+	}
+		 
+	public int getValorDeCarta(String carta) {
+
+		int valorDeLaCarta; 
+		
+		if(carta.length() == 3) {
+			valorDeLaCarta = 10;
+		}
+		else {
+			valorDeLaCarta = (int)carta.charAt(0);
+		}
+		return valorDeLaCarta;
+	}
+	
+	public Character getPaloDeCarta(String carta) {
+		return carta.charAt(carta.length()-1);
+	}
 	
 	
 	
--- TP-Doubles/test/poker/test/PokerStatusTest.java
+++ TP-Doubles/test/poker/test/PokerStatusTest.java
@@ -1,18 +1,69 @@
 package poker.test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import poker.src.PokerStatus;
+
 public class PokerStatusTest {
 	
+	// Exercise 
+	String carta5P = "5P";
+	String carta10D = "10D";
+	String carta5C = "5C";
+	String carta5D = "5D";
+	String carta5T = "5T";
+	String carta3D = "3D";
+	String carta1D = "1D";
 	
 	
 	
+	PokerStatus juegoDeMesa;
+	
+	@BeforeEach
+	public void setUp() {
 		
+		juegoDeMesa = new PokerStatus();
+		
+	}
 	
+	@Test 
+    public void test01_hayDosCartasDeMismoNumero() {
+		assertTrue(juegoDeMesa.esElMismoNumero(carta5P,carta5T));
+	}
 	
+	@Test 
+    public void test02_hayCuatroCartasDeMismoNumero() {
+		assertTrue(juegoDeMesa.tieneMismoNumero(carta10D,carta5T,carta5P,carta5C,carta5D));
+	}
 	
+	@Test 
+    public void test03_hayDosCartasDeMismoPalo() {
+		assertTrue(juegoDeMesa.esElMismoPalo(carta10D,carta5D));
+	}	
 	
+	@Test 
+    public void test04_NoHayCuatroCartasDeMismoPalo() {
+		assertFalse(juegoDeMesa.tieneMismoPalo(carta10D,carta5T,carta5P,carta5C,carta5D)); // Verify
+	}
 	
+	@Test 
+    public void test05_HayCuatroCartasDeMismoPalo() {
+		assertTrue(juegoDeMesa.tieneMismoPalo(carta10D,carta1D,carta3D,carta5C,carta5D));
+	}
 	
+	@Test 
+    public void test06_cuandoHayPokerPorNumero() {
+		assertTrue(juegoDeMesa.verificar(carta10D,carta5P,carta5C,carta5T,carta5D));
+	}
 	
+	@Test 
+    public void test07_cuandoHayPokerPorPalo() {
+		assertTrue(juegoDeMesa.verificar(carta10D,carta1D,carta3D,carta5T,carta5D));
+	}
 	
 	
 	
